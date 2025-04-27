993497f46662e32ac56f46ddb8b2598c2c248177
Faizan Saleh <116947935+FaizanMirza123@users.noreply.github.com>
2025-04-27 12:14:25 +0500

    Muneeb merged

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,15 @@ import Index from "./Pages/Index";
 // import DDoSAttack from "./pages/DDoSAttack";
 import NotFound from "./Pages/NotFound";
 import Navbar from "./Navbar";
-
+import XSSAttack from "./Pages/XSSAttack";
 const App = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Navbar />}>
         <Route index element={<Index />} />
-        {/* <Route path="sql-injection" element={<SQLInjection />} />
         <Route path="xss" element={<XSSAttack />} />
+        {/* <Route path="sql-injection" element={<SQLInjection />} />
+     
         <Route path="ddos" element={<DDoSAttack />} />*/}
         <Route path="*" element={<NotFound />} /> 
       </Route>
