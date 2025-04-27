497287fb13763ffcd45939431295a4020a39e665
Faizan Saleh <116947935+FaizanMirza123@users.noreply.github.com>
2025-04-27 13:25:14 +0500

    ddos merged

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,17 @@ import Index from "./Pages/Index";
 import NotFound from "./Pages/NotFound";
 import Navbar from "./Navbar";
 import XSSAttack from "./Pages/XSSAttack";
+import DdosPage from "./Pages/ddosPage";
 const App = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Navbar />}>
         <Route index element={<Index />} />
+        <Route path="ddos" element={<DdosPage />}/>
         <Route path="xss" element={<XSSAttack />} />
         {/* <Route path="sql-injection" element={<SQLInjection />} />
      
-        <Route path="ddos" element={<DDoSAttack />} />*/}
+        />*/}
         <Route path="*" element={<NotFound />} /> 
       </Route>
     </Routes>
